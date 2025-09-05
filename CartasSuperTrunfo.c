0ade0a851f881d2fdc2f15c26adabc831101eed0
ebertgoncalves2311-sys <ebertgoncalves2311@gmail.com>
2025-09-05 13:55:07 +0000

    pedindo dados do usuario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,18 +7,20 @@ printf("Seja bem vindo ao jogo de cartas Super Trunfo!\n");
 
 //Definindo variáveis da primeira carta
 
-char estado1;
-char nome_cidade1[20], nomecidade1[20], codigocarta1;
+char nome_cidade1[20], nomecidade1[20], codigocarta1, estado1;
 int populacao1, pontos_turisticos1;
 float area1, pib1;
 
 //Definindo variáveis da segunda carta
-char estado2;
-char nome_cidade2[20], nomecidade2[20], codigocarta2;
+
+char nome_cidade2[20], nomecidade2[20], codigocarta2, estado2;
 int populacao2, pontos_turisticos2;
 float area2, pib2;
 
-//Pedindo dados do usuario
+//Cadastro de primeira carta
+
+printf("Digite o estado(A-H):   ");
+scanf("s",   estado1);
 
 }
 
