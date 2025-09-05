7931d557418aa0e0306f8528bfa6b6526691ed5a
ebertgoncalves2311-sys <ebertgoncalves2311@gmail.com>
2025-09-05 13:31:25 +0000

    declarando variaveis carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,6 +3,13 @@
 int main(){
 //Mensagem de boas vindas
 printf("Seja bem vindo ao jogo de cartas Super Trunfo!\n");
+//Definindo variáveis da primeira carta
+int populacao1, pontos_turisticos1;
+float area1, pib1;
+
+ 
+
+
 
 
 
