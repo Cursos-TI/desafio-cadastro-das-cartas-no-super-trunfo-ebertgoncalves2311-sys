1faec50a547a11527489583dcbdcacc82d659a50
ebertgoncalves2311-sys <ebertgoncalves2311@gmail.com>
2025-09-05 13:33:24 +0000

    declarando variáveis carta dois

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,10 @@ printf("Seja bem vindo ao jogo de cartas Super Trunfo!\n");
 //Definindo variáveis da primeira carta
 int populacao1, pontos_turisticos1;
 float area1, pib1;
+//Definindo variáveis da segunda carta
+int populacao2, pontos_turisticos2;
+float area2, pib2;
+
 
  
 
